d781b83a2f308973aaaa5768db17422d32f22c1b
muzierri <229398570@qq.com>
2019-09-04 17:52:11 +0800

    Merge branch 'master' of https://github.com/muzierri/project2 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/ly/test1.java
+++ src/main/java/com/ly/test1.java
@@ -4,6 +4,7 @@ package com.ly;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("i love you");
-        System.out.println("i love you");
+        System.out.println("i love you2");
+        System.out.println("i love you3");
     }
 }
